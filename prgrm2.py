0f7a78ccb555cd30403fbdca747336e858dd2283
hadlaaa <hadilapaingeery111@gmail.com>
2026-09-22 15:39:57 +0530

    new

---
+++ prgrm2.py
@@ -0,0 +1,7 @@
+n=input("enter the number:")
+if 'j'in n:
+ print("complex")
+elif'.'in n:
+ print("float")
+else:
+ print("integer")
---
+++ prgrm4.py
@@ -0,0 +1,9 @@
+basic_pay=float(input("enter the basic pay:"))
+HRA=10/100*basic_pay
+TA=5/100*basic_pay
+salary=basic_pay+HRA+TA
+print("basic pay=",basic_pay)
+print("HRA=",HRA)
+print("TA=",TA)
+print("total salary=",salary)
+
